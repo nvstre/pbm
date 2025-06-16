c996bee091e92829281b25b60b3149225393a713
nvstre <mbiraut@gmail.com>
2025-06-16 22:35:49 +0200

    Vercel fix

--- src/components/animated-shader-background.tsx
+++ src/components/animated-shader-background.tsx
@@ -87,11 +87,11 @@ const AnoAI = () => {
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-    let frameId;
+    let frameId: 19;
     const animate = () => {
       material.uniforms.iTime.value += 0.016;
       renderer.render(scene, camera);
-      frameId = requestAnimationFrame(animate);
+      frameId = window.requestAnimationFrame(animate) as unknown as 19;
     };
     animate();
 
