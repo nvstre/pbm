0b6185049941f1ed9221dabded9e8d505a62d78f
nvstre <mbiraut@gmail.com>
2025-06-17 19:42:36 +0200

    Background fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export default function FloatingPathsBackgroundExample() {
       className="aspect-16/9 flex items-center justify-center"
       position={-1}
     >
+        {/* Child component or content here */}
+         <div />
     </FloatingPathsBackground>
   );
 }
